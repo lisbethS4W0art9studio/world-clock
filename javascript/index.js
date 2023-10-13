f3a2e8b7a06404d0bb8ddaa5e1042994d9a51bf3
Lisbeth Stafferton <131831523+lisbethS4W0art9studio@users.noreply.github.com>
2023-10-13 18:28:13 +1100

    removed sydney elements

--- javascript/index.js
+++ javascript/index.js
@@ -7,11 +7,3 @@ belfastDateElement.innerHTML = belfastTime.format("MMMM Do YYYY");
 belfastTimeElement.innerHTML = belfastTime.format(
   "h:mm:ss [<small>]A[</small>"
 );
-
-// Sydney
-let sydneyElement = document.querySelector("#sydney");
-let sydneyDateElement = sydneyElement.querySelector(".date");
-let sydneyTimeElement = sydneyElement.querySelector(".time");
-let sydneyTime = moment().tz("Australia/Sydney");
-sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
-sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>]A[</small>]");
