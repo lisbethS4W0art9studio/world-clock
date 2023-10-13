33dc784d69ff533dc66a65855659828535b80384
Lisbeth Stafferton <131831523+lisbethS4W0art9studio@users.noreply.github.com>
2023-10-13 18:27:10 +1100

    removed function element

--- javascript/index.js
+++ javascript/index.js
@@ -1,24 +1,17 @@
-function updateTime() {
-  //Belfast
-  let belfastElement = document.querySelector("#belfast");
-  let belfastDateElement = belfastElement.querySelector(".date");
-  let belfastTimeElement = belfastElement.querySelector(".time");
-  let belfastTime = moment().tz("Europe/Belfast");
-  belfastDateElement.innerHTML = belfastTime.format("MMMM Do YYYY");
-  belfastTimeElement.innerHTML = belfastTime.format(
-    "h:mm:ss [<small>]A[</small>"
-  );
+//Belfast
+let belfastElement = document.querySelector("#belfast");
+let belfastDateElement = belfastElement.querySelector(".date");
+let belfastTimeElement = belfastElement.querySelector(".time");
+let belfastTime = moment().tz("Europe/Belfast");
+belfastDateElement.innerHTML = belfastTime.format("MMMM Do YYYY");
+belfastTimeElement.innerHTML = belfastTime.format(
+  "h:mm:ss [<small>]A[</small>"
+);
 
-  // Sydney
-  let sydneyElement = document.querySelector("#sydney");
-  let sydneyDateElement = sydneyElement.querySelector(".date");
-  let sydneyTimeElement = sydneyElement.querySelector(".time");
-  let sydneyTime = moment().tz("Australia/Sydney");
-  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
-  sydneyTimeElement.innerHTML = sydneyTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-}
-
-updateTime();
-setInterval(updateTime, 1000);
+// Sydney
+let sydneyElement = document.querySelector("#sydney");
+let sydneyDateElement = sydneyElement.querySelector(".date");
+let sydneyTimeElement = sydneyElement.querySelector(".time");
+let sydneyTime = moment().tz("Australia/Sydney");
+sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>]A[</small>]");
