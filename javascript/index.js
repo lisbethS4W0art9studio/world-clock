254069e75679d48c2189677b459e1d2c25a4b389
Lisbeth Stafferton <131831523+lisbethS4W0art9studio@users.noreply.github.com>
2023-10-13 18:48:28 +1100

    added Sydney script

--- javascript/index.js
+++ javascript/index.js
@@ -7,3 +7,11 @@ belfastDateElement.innerHTML = belfastTime.format("MMMM Do YYYY");
 belfastTimeElement.innerHTML = belfastTime.format(
   "h:mm:ss [<small>]A[</small>]"
 );
+
+// Sydney
+let sydneyElement = document.querySelector("#sydney");
+let sydneyDateElement = sydneyElement.querySelector(".date");
+let sydneyTimeElement = sydneyElement.querySelector(".time");
+let sydneyTime = moment().tz("Australia/Sydney");
+sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>]A[</small>]");
