2f9ef59c4c2e1f15e1bb1f7100c0907d2ffa0440
Lisbeth Stafferton <131831523+lisbethS4W0art9studio@users.noreply.github.com>
2023-10-20 17:57:51 +1100

    Added more cities

--- javascript/index.js
+++ javascript/index.js
@@ -21,6 +21,28 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+  // Dublin
+  let dublinElement = document.querySelector("#dublin");
+  if (dublinElement) {
+    let dublinDateElement = dublinElement.querySelector(".date");
+    let dublinTimeElement = dublinElement.querySelector(".time");
+    let dublinTime = moment().tz("Europe/Dublin");
+    dublinDateElement.innerHTML = dublinTime.format("MMMM Do YYYY");
+    dublinTimeElement.innerHTML = dublinTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+  // Mexico City
+  let mexicoCityElement = document.querySelector("#mexico_city");
+  if (mexicoCityElement) {
+    let mexicoCityDateElement = mexicoCityElement.querySelector(".date");
+    let mexicoCityTimeElement = mexicoCityElement.querySelector(".time");
+    let mexicoCityTime = moment().tz("America/Mexico_City");
+    mexicoCityDateElement.innerHTML = mexicoCityTime.format("MMMM Do YYYY");
+    mexicoCityTimeElement.innerHTML = mexicoCityTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
